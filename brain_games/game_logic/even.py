dab32f01d1eeb3e0af1a923810b1b267ee85a608
Dmitry Nov <pieero47@gmail.com>
2023-01-22 23:28:48 +0300

    fix even

--- brain_games/game_logic/even.py
+++ brain_games/game_logic/even.py
@@ -4,7 +4,6 @@ START = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def logic_function():
     random_number = randint(1, 100)
-    result = 'error'
     if random_number % 2 == 0:
         result = 'yes'
     elif random_number % 2 != 0:
